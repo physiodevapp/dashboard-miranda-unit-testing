91235c1f48513721e5e2f833ee4c087ba79122d5
physiodevapp <edu.gamboa.rodriguez@gmail.com>
2024-07-09 14:21:02 +0200

    test: add invalid date format case for isOccupied method

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
 
+function checkDate(dateString) {
+  const regex = /^(\d{2})\/(\d{2})\/(\d{4})$/;
+
+  const match = dateString.match(regex);
+
+  const result = {
+    isValid: true,
+    error: null
+  }
+
+  if (!match) {
+    result.error = new Error('Invalid date format. Please use dd/mm/yyyy.');
+    result.isValid = false;
+  }
+
+  const day = parseInt(match[1], 10);
+  const month = parseInt(match[2], 10) - 1;
+  const year = parseInt(match[3], 10);
+
+  const date = new Date(year, month, day);
+
+  if (date.getFullYear() !== year || date.getMonth() !== month || date.getDate() !== day) {
+    result.error = new Error('Invalid date. Please check the day, month, and year.');
+    result.isValid = false;
+  }
+
+  return result;
+}
+
+
 class Room {
 
   constructor({name, bookingList, rate, discount}) {
@@ -8,12 +38,16 @@ class Room {
     this.discount = discount;
   }
 
+  isOccupied(dateString) {     
+    const { isValid, error } = checkDate(dateString);
 
-  isOccupied(date) {    
-    return !!this.bookingList.find((booking) => (
-      date >= booking.checkIn && 
-      date < booking.checkOut
-    ));      
+    if (isValid)
+      return !!this.bookingList.find((booking) => (
+        dateString >= booking.checkIn && 
+        dateString < booking.checkOut
+      ));  
+    else 
+      throw error
   };
 }
 
--- index.test.js
+++ index.test.js
@@ -6,8 +6,7 @@ const roomTemplate = { name: "Suite A", rate: 4, discount: 5 };
 
 describe("Room tests", () => {
 
-  test('isOccupied Room method', () => {
-    
+  test('Room is occupied', () => {    
     const room = new Room({...roomTemplate});
     const booking1 = new Booking({...bookingTemplate, room, checkIn: "09/07/2024", checkOut: "10/07/2024"});
     const booking2 = new Booking({...bookingTemplate, room, checkIn: "12/07/2024", checkOut: "20/07/2024"});
@@ -16,7 +15,28 @@ describe("Room tests", () => {
     const isOccupied = room.isOccupied("19/07/2024");
 
     expect(isOccupied).toBe(true);
-
   });
-  
+
+  test('Room is not occupied', () => {    
+    const room = new Room({...roomTemplate});
+    const booking1 = new Booking({...bookingTemplate, room, checkIn: "09/07/2024", checkOut: "10/07/2024"});
+    const booking2 = new Booking({...bookingTemplate, room, checkIn: "12/07/2024", checkOut: "20/07/2024"});
+    room.bookingList = [booking1, booking2];
+
+    const isOccupied = room.isOccupied("20/07/2024");
+
+    expect(isOccupied).toBe(false);
+  });  
+
+  test('Invalid date for isOccupied method', () => {    
+    const room = new Room({...roomTemplate});
+    const booking1 = new Booking({...bookingTemplate, room, checkIn: "09/07/2024", checkOut: "10/07/2024"});
+    const booking2 = new Booking({...bookingTemplate, room, checkIn: "12/07/2024", checkOut: "20/07/2024"});
+    room.bookingList = [booking1, booking2];
+
+
+
+    expect(() => room.isOccupied("fdsafads")).toThrow()
+  });  
+
 });
