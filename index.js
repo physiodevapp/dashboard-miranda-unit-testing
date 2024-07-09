9da303ee38b94c1ef55ff90be17297a3fdc406d3
physiodevapp <edu.gamboa.rodriguez@gmail.com>
2024-07-09 18:38:45 +0200

    test: add testing for occupancyPercentage method

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-
-function checkDate(dateString) {
+const checkDate = (dateString) => {
   const regex = /^(\d{2})\/(\d{2})\/(\d{4})$/;
 
   const match = dateString.match(regex);
 
   if (!match) {
-    throw new Error('Invalid date format. Please use dd/mm/yyyy.');
+    throw new Error("Invalid date format. Please use dd/mm/yyyy.");
   }
 
   const day = parseInt(match[1], 10);
@@ -14,40 +13,85 @@ function checkDate(dateString) {
 
   const date = new Date(year, month, day);
 
-  if (date.getFullYear() !== year || date.getMonth() !== month || date.getDate() !== day) {
-    throw new Error('Invalid date. Please check the day, month, and year.');
+  if (
+    date.getFullYear() !== year ||
+    date.getMonth() !== month ||
+    date.getDate() !== day
+  ) {
+    throw new Error("Invalid date. Please check the day, month, and year.");
   }
 
   return date;
-}
+};
 
+const getDatesBetween = (currentDate, lastDate) => {
+  const dates = [];
+
+  if (currentDate > lastDate) {
+    throw new Error("End date must be after start date");
+  }
+
+  while (currentDate <= lastDate) {
+    let options = {
+      timeZone: "Europe/Madrid",
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+    };
+    const newDate = new Date(currentDate).toLocaleDateString("es-ES", options);
+    dates.push(newDate);
+
+    currentDate.setDate(currentDate.getDate() + 1);
+  }
+
+  return dates;
+};
 
 class Room {
-
-  constructor({name, bookingList, rate, discount}) {
+  constructor({ name, bookingList, rate, discount }) {
     this.name = name;
     this.bookingList = bookingList;
     this.rate = rate;
     this.discount = discount;
   }
 
-  isOccupied(dateString) {   
+  isOccupied(dateString) {
     try {
       checkDate(dateString);
 
-      return !!this.bookingList.find((booking) => (
-        dateString >= booking.checkIn && 
-        dateString < booking.checkOut
-      ));  
+      return !!this.bookingList.find(
+        (booking) =>
+          dateString >= booking.checkIn && dateString < booking.checkOut
+      );
     } catch (error) {
-      throw error
+      throw error;
     }
-  };
+  }
+
+  occupancyPercentage(startDateString, endDateString) {
+    try {
+      const betweenDates = [startDateString, endDateString].map((dateString) =>
+        checkDate(dateString)
+      );
+      const currentDate = new Date(betweenDates[0]);
+      const lastDate = new Date(betweenDates[1]);
+      const dates = getDatesBetween(currentDate, lastDate);
+
+      return Math.round(
+        (dates
+          .map((date) => this.isOccupied(date))
+          .filter((isOccupied) => isOccupied === true).length /
+          dates.length) *
+          100
+      );
+    } catch (error) {
+      throw error;
+    }
+  }
 }
 
 class Booking {
-
-  constructor({name, email, checkIn, checkOut, discount, room}) {
+  constructor({ name, email, checkIn, checkOut, discount, room }) {
     this.name = name;
     this.email = email;
     this.checkIn = checkIn;
@@ -55,14 +99,9 @@ class Booking {
     this.discount = discount;
     this.room = room;
   }
-
 }
-
-
-
-
 
 module.exports = {
   Room,
-  Booking
-}
+  Booking,
+};
